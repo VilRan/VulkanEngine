27c1fa42f19151a7818ddd755539c857c9e8dbe9
Ville Matti Rantala <VilleMattiRantala@gmail.com>
2017-05-28 17:36:08 +0300

    Make sprites centered

--- VulkanEngine/SpriteManager.cpp
+++ VulkanEngine/SpriteManager.cpp
@@ -30,22 +30,24 @@ Sprite* SpriteManager::Create(Texture* texture)
 
 Sprite* SpriteManager::Create(Texture* texture, Rectangle area)
 {
+	float width = area.GetWidth() / 2;
+	float height = area.GetHeight() / 2;
 	float left = area.GetLeft() / (float)texture->GetWidth();
 	float right = area.GetRight() / (float)texture->GetWidth();
 	float top = area.GetTop() / (float)texture->GetHeight();
 	float bottom = area.GetBottom() / (float)texture->GetHeight();
 
 	std::vector<Vertex> vertices(4);
-	vertices[0].Position = glm::vec3(0.0f, 0.0f, 0.0f);
+	vertices[0].Position = glm::vec3(-width, -height, 0.0f);
 	vertices[0].TextureCoordinates = glm::vec3(left, top, 0.0f);
 	vertices[0].Color = glm::vec3(1.0f, 1.0f, 1.0f);
-	vertices[1].Position = glm::vec3(area.GetWidth(), 0.0f, 0.0f);
+	vertices[1].Position = glm::vec3(width, -height, 0.0f);
 	vertices[1].TextureCoordinates = glm::vec3(right, top, 0.0f);
 	vertices[1].Color = glm::vec3(1.0f, 1.0f, 1.0f);
-	vertices[2].Position = glm::vec3(0.0f, area.GetHeight(), 0.0f);
+	vertices[2].Position = glm::vec3(-width, height, 0.0f);
 	vertices[2].TextureCoordinates = glm::vec3(left, bottom, 0.0f);
 	vertices[2].Color = glm::vec3(1.0f, 1.0f, 1.0f);
-	vertices[3].Position = glm::vec3(area.GetWidth(), area.GetHeight(), 0.0f);
+	vertices[3].Position = glm::vec3(width, height, 0.0f);
 	vertices[3].TextureCoordinates = glm::vec3(right, bottom, 0.0f);
 	vertices[3].Color = glm::vec3(1.0f, 1.0f, 1.0f);
 
